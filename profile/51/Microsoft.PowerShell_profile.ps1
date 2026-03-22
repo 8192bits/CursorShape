80f2ea74dd2f9be259ddeb55023395f15b6ae9c9
8192bits <vwin32@tutanota.de>
2026-03-22 19:29:34 +0100

    Delete profile directory

--- profile/51/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Clear-Host
-Write-Host "Windows PowerShell 5.1"
-Write-Host "Copyright (C) Microsoft Corporation. All rights reserved."
-Write-Host ""
-
-Import-Module "$HOME\Documents\WindowsPowerShell\Modules\PSReadLine\2.4.5\PSReadLine.psd1" -Force
--- profile/7x/Microsoft.PowerShell_profile.ps1
+++
@@ -1,1 +0,0 @@
-Import-Module "$HOME\Documents\PowerShell\Modules\PSReadLine\2.4.5\PSReadLine.psd1" -Force
